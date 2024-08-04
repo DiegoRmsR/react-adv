c94eeff9c8eb0786181aa795f26e079051a2b59d
DiegoRmsR <diegoramos.mt@gmail.com>
2024-08-04 00:20:21 -0500

    Control props implemented

---
+++ src/02-patterns-components/data/products.ts
@@ -0,0 +1,24 @@
+import { Product } from "../interfaces/interfaces";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        title: "Coffe Mug",
+        img: "./coffee-mug.png",
+    },
+    {
+        id: 2,
+        title: "Coffe Mug",
+        img: "./coffee-mug2.png",
+    },
+    {
+        id: 3,
+        title: "Coffe Mug3",
+        img: "./coffee-mug.png",
+    },
+    {
+        id: 4,
+        title: "Coffe Mug4",
+        img: "./coffee-mug2.png",
+    },
+];
